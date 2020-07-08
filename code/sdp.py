d5a6297fdca44013663f85ee14a3b0352d675c4a
taberlasche <luca.goecke@protonmail.com>
2020-07-08 17:09:13 +0200

    remove cvxopt attempt, added hermitian constraint and syntax

--- code/sdp.py
+++ code/sdp.py
@@ -2,15 +2,15 @@ import picos as pic
 import cvxopt as cvx
 import cvxopt.lapack
 import numpy as np
-def mc(C)
+def mc(C):
     N = C.size[0]
     maxcut = pic.Problem()
 
     # Add the symmetric matrix variable.
-    X=maxcut.add_variable('X', (N,N), 'symmetric')
+    X=maxcut.add_variable('X', (N,N), 'hermitian')
 
     # Constrain X to have ones on the diagonal.
-    maxcut.add_constraint(pic.diag_vect(X) == 1)
+    maxcut.add_constraint(pic.maindiag(X) == 1)
 
     # Constrain X to be positive semidefinite.
     maxcut.add_constraint(X >> 0)
@@ -22,6 +22,5 @@ def mc(C)
 
     # Solve the problem.
     maxcut.solve(solver='cvxopt')
-
-    print('.format(maxcut.obj_value()))
+    print('bound from the SDP relaxation: {0}'.format(maxcut.obj_value()))
     print(X)
