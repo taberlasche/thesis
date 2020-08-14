800285bc019bc58d094dd189adb15d708ed7ffb7
taberlasche <luca.goecke@protonmail.com>
2020-08-14 14:06:49 +0200

    plot

--- code/test.py
+++ code/test.py
@@ -16,25 +16,6 @@ def roundeig(M):
         if w[i]<0:
             if abs(w[i])<10e-8: w[i]=0
     return w
-<<<<<<< HEAD
 
-x=[]
-y=[]
-for i in range(2,20):
-    x.append(i)
-    y.append(tfisingmaxeig(1,1,i))
-plt.scatter(x,y,marker=".",s=3)
-plt.xlabel('n')
-plt.ylabel('maxeig')
-#plt.ylabel('log(ratio)')
-#plt.yscale('log')
-plt.title('nplot')
-plt.show()
-=======
-print(tfisdp(4))
-print(tfisdp(6))
-print(tfisdp(8))
-print(tfisdp(10))
-print(tfisdp(12))
-print(tfisdp(14))
->>>>>>> 6e8cda01c29b9e9c46601cc4f0c8b812c9fe58d1
+
+tfiplot(20,4000,1,15,10,2,3)
