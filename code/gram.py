5e9493726e78c69bac0fffd1ed512d785285a39c
taberlasche <luca.goecke@protonmail.com>
2020-09-12 13:24:18 +0200

    code and plots

--- code/gram.py
+++ code/gram.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse.linalg
 
 
 def findGenVecGram(gram):
--- code/test.py
+++ code/test.py
@@ -17,4 +17,4 @@ def roundeig(M):
             if abs(w[i])<10e-8: w[i]=0
     return w
 
-tfiplot(4,2000,1,20,5,2,4)
+tfiplot(4,5200,2,20,25,2,4)
